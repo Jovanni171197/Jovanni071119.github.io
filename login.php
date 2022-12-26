72fcfc2a7fc9069f3337735e5c3fc476f18dd0a4
Jovanni171197 <jhonfaibautista@gmail.com>
2022-12-25 21:52:34 -0600

    secretaria De Cultura

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "test";
+$conn =mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if(!$conn)
+{
+    die("No hay conexión:".mysqli_connect_error());
+}
+$nombre = $_POST ["txtusuario"];
+$pass = $_POST ["txtpassword"];
+
+$query = mysqli_query($conn,"SELECT * FROM login WHERE usuario = '".$nombre."' and password = '".$pass."'");
+$nr = mysqli_num_rows($query);
+
+if($nr == 1)
+{
+    //header("location: pagina.html")
+    echo "Bienvenido:" .$nombre;
+}
+{
+    echo "No ingreso";
+}
+
+?>
